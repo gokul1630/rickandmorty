a473eaa52b28f838b0fd96ba66d1dfb4d20a8731
gokul1630 <gokulgp3167@gmail.com>
2022-12-23 15:35:28 +0530

    initial setup with vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface AppProps {
+  
+};
+
+const App = (props: AppProps) => {
+  const {} = props
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App
