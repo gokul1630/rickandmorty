010a7770318ad0eee685e0847124d853d6022464
gokul1630 <gokulgp3167@gmail.com>
2022-12-24 12:00:26 +0530

    Added Eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
+  rules: {
+    'react/prop-types': 0,
+    indent: ['error', 2],
+    'linebreak-style': 1,
+    quotes: ['error', 'single'],
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn'],
+      },
+    ],
+    'no-unused-vars': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
+      },
+    ],
+    camelcase: [
+      'error',
+      {
+        allow: ['unstable_batchedUpdates'],
+      },
+    ],
+    'react/require-default-props': 'off',
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
