603c60ee1ff3182c107d6398e91de8d6dd3fcb49
gokul1630 <gokulgp3167@gmail.com>
2022-12-24 13:14:00 +0530

    Downgraded react version

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-);
+ReactDOM.render(<App />, document.getElementById('root'));
