a587c0110417cf5996d901361e72c7566e597a3e
gokul1630 <gokulgp3167@gmail.com>
2022-12-24 12:03:08 +0530

    Added Home & Character Pages & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+const Character = lazy(() => import('./Components/Character'));
+const Home = lazy(() => import('./Components/Home'));
 
 function App() {
   return (
     <Suspense>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:id" element={<Character />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
