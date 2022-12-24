086d03b82134a58004011ea0b53c6024c44586f3
gokul1630 <gokulgp3167@gmail.com>
2022-12-24 12:18:01 +0530

    Added fallback Loader.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Loader from './Components/Loader';
 
 const Character = lazy(() => import('./Components/Character'));
 const Home = lazy(() => import('./Components/Home'));
 
 function App() {
   return (
-    <Suspense>
+    <Suspense fallback={<Loader />}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
