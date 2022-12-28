fb83281d3046f854b49e9018c070c5c869dafce8
gokul1630 <gokulgp3167@gmail.com>
2022-12-28 11:08:18 +0530

    Added UseTransition() Hook

--- src/Pages/Character.tsx
+++ src/Pages/Character.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useTransition } from 'react';
 import { useLocation } from 'react-router-dom';
+import Loader from '../Components/Loader';
 
 function Character() {
   const [episodeData, setEpisodeData] = useState<any[]>([]);
@@ -8,18 +9,22 @@ function Character() {
     dimension: '',
     residents: [],
   });
+  const [isLoading, startTransition] = useTransition();
 
   const fetchEpisodes = async (episode: string) => {
     const response = await fetch(episode);
     const data = await response.json();
-    setEpisodeData((state) => [...state, data.name]);
+    startTransition(() => {
+      setEpisodeData((state) => [...state, data.name]);
+    });
   };
 
   const fetchLocations = async (episode: string) => {
     const response = await fetch(episode);
     const data = await response.json();
-
-    setLocationsData(data);
+    startTransition(() => {
+      setLocationsData(data);
+    });
   };
 
   const { state } = useLocation() as any;
@@ -42,34 +47,40 @@ function Character() {
         src={image}
         className="rounded-md object-cover self-start w-full md:max-w-md h-auto shadow-md hover:shadow-xl"
       />
-      <div>
+      {!isLoading ? (
         <div>
-          {name && <h4 className="text-xl">{`Name: ${name}`}</h4>}
-          {species && <p className="text-xl mt-2">{`Species: ${species}`}</p>}
-          {origin.name && (
-            <p className="text-xl mt-2">{`Origin: ${origin.name}`}</p>
-          )}
-          {locationsData.name && (
-            <p className="text-xl mt-2">{`Dimension: ${locationsData.dimension}`}</p>
-          )}
-          {locationsData.name && (
-            <p className="text-xl mt-2">{`Location: ${locationsData.name}`}</p>
-          )}
-          {locationsData.residents && (
-            <p className="text-xl mt-2">{`Amount of Residents: ${locationsData.residents.length}`}</p>
-          )}
+          <div>
+            {name ? <h4 className="text-xl">{`Name: ${name}`}</h4> : null}
+            {species ? (
+              <p className="text-xl mt-2">{`Species: ${species}`}</p>
+            ) : null}
+            {origin.name ? (
+              <p className="text-xl mt-2">{`Origin: ${origin.name}`}</p>
+            ) : null}
+            {locationsData.name ? (
+              <p className="text-xl mt-2">{`Dimension: ${locationsData.dimension}`}</p>
+            ) : null}
+            {locationsData.name ? (
+              <p className="text-xl mt-2">{`Location: ${locationsData.name}`}</p>
+            ) : null}
+            {locationsData.residents ? (
+              <p className="text-xl mt-2">{`Amount of Residents: ${locationsData.residents.length}`}</p>
+            ) : null}
+          </div>
+          <h1 className="text-2xl lg:text-3xl text-blue-500 mt-4">
+            Episodes Appeared
+          </h1>
+          <ol className="mt-4">
+            {episodeData?.map((episode, idx) => (
+              <li key={episode} className="text-lg lg:text-xl text-gray-800">
+                {idx + 1}. {episode}
+              </li>
+            ))}
+          </ol>
         </div>
-        <h1 className="text-2xl lg:text-3xl text-blue-500 mt-4">
-          Episodes Appeared
-        </h1>
-        <ol className="mt-4">
-          {episodeData?.map((episode, idx) => (
-            <li key={episode} className="text-lg lg:text-xl text-gray-800">
-              {idx + 1}. {episode}
-            </li>
-          ))}
-        </ol>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useTransition } from 'react';
 import { Link } from 'react-router-dom';
+import Loader from '../Components/Loader';
 import { Pagination } from '../Components/Pagination';
 
 function Home() {
   const [data, setData] = useState<any>({});
+  const [isLoading, startTransition] = useTransition();
 
   const fetchCharacters = async (page?: number) => {
     const response = await fetch(
@@ -11,8 +13,9 @@ function Home() {
     );
 
     const parsedData = await response.json();
-
-    if (response.ok) setData(parsedData);
+    startTransition(() => {
+      if (response.ok) setData(parsedData);
+    });
   };
 
   useEffect(() => {
@@ -34,69 +37,76 @@ function Home() {
       <h1 className="text-blue-500 py-10 text-4xl text-center font-bold">
         Rick And Morty
       </h1>
-      <ul className="list-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
-        {results?.map((datas: any) => {
-          const {
-            id,
-            name,
-            species,
-            gender,
-            origin,
-            location,
-            image,
-            episode,
-          } = datas;
-          return (
-            <li
-              key={id}
-              className="p-4 border rounded-xl shadow-md hover:shadow-xl"
-            >
-              <Link
-                state={{
-                  episode: JSON.stringify(episode),
-                  user: {
-                    name,
-                    species,
-                    image,
-                    origin,
-                    location,
-                  },
-                }}
-                to={`character/${id}`}
-              >
-                <img
-                  alt={name}
-                  src={image}
-                  className="rounded-lg object-cover w-full h-auto"
-                />
-                <div className="mt-5 flex gap-2">
-                  <div>
-                    {name && <p>Name: </p>}
-                    {species && <p>Species: </p>}
-                    {gender && <p>Gender: </p>}
-                    {origin.name && <p>Origin: </p>}
-                    {location.name && <p>Location: </p>}
-                  </div>
-                  <div>
-                    {name && <h4> {name}</h4>}
-                    {species && <p> {species}</p>}
-                    {gender && <p>{gender}</p>}
-                    {origin.name && <p>{origin.name}</p>}
-                    {location.name && <p> {location.name}</p>}
-                  </div>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      {results && info && (
-        <Pagination
-          pageCount={info.count}
-          className="py-10 w-full"
-          pageSize={20}
-          onPageChange={onPageChange}
-        />
+
+      {!isLoading ? (
+        <>
+          <ul className="list-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
+            {results?.map((datas: any) => {
+              const {
+                id,
+                name,
+                species,
+                gender,
+                origin,
+                location,
+                image,
+                episode,
+              } = datas;
+              return (
+                <li
+                  key={id}
+                  className="p-4 border rounded-xl shadow-md hover:shadow-xl"
+                >
+                  <Link
+                    state={{
+                      episode: JSON.stringify(episode),
+                      user: {
+                        name,
+                        species,
+                        image,
+                        origin,
+                        location,
+                      },
+                    }}
+                    to={`character/${id}`}
+                  >
+                    <img
+                      alt={name}
+                      src={image}
+                      className="rounded-lg object-cover w-full h-auto"
+                    />
+                    <div className="mt-5 flex gap-2">
+                      <div>
+                        {name ? <p>Name: </p> : null}
+                        {species ? <p>Species: </p> : null}
+                        {gender ? <p>Gender: </p> : null}
+                        {origin.name ? <p>Origin: </p> : null}
+                        {location.name ? <p>Location: </p> : null}
+                      </div>
+                      <div>
+                        {name ? <h4> {name}</h4> : null}
+                        {species ? <p> {species}</p> : null}
+                        {gender ? <p>{gender}</p> : null}
+                        {origin.name ? <p>{origin.name}</p> : null}
+                        {location.name ? <p> {location.name}</p> : null}
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          {info ? (
+            <Pagination
+              pageCount={info.count}
+              className="py-10 w-full"
+              pageSize={20}
+              onPageChange={onPageChange}
+            />
+          ) : null}
+        </>
+      ) : (
+        <Loader />
       )}
     </div>
   );
