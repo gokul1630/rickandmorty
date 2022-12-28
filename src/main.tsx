151ed426be0bf07fe963a0a92133248f6a7c4c1d
gokul1630 <gokulgp3167@gmail.com>
2022-12-28 11:14:05 +0530

    Revert "Downgraded react version"
    This reverts commit 603c60ee1ff3182c107d6398e91de8d6dd3fcb49.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+);
