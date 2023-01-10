6987b40e3045ca2407bef778f9f90587e07b1db9
gokul1630 <gokulgp3167@gmail.com>
2023-01-10 15:55:55 +0530

    Fixed Pagination component unmounts on Transition update.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -39,75 +39,73 @@ function Home() {
       </h1>
 
       {!isLoading ? (
-        <>
-          <ul className="list-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
-            {results?.map((datas: any) => {
-              const {
-                id,
-                name,
-                species,
-                gender,
-                origin,
-                location,
-                image,
-                episode,
-              } = datas;
-              return (
-                <li
-                  key={id}
-                  className="p-4 border rounded-xl shadow-md hover:shadow-xl"
+        <ul className="list-none grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
+          {results?.map((datas: any) => {
+            const {
+              id,
+              name,
+              species,
+              gender,
+              origin,
+              location,
+              image,
+              episode,
+            } = datas;
+            return (
+              <li
+                key={id}
+                className="p-4 border rounded-xl shadow-md hover:shadow-xl"
+              >
+                <Link
+                  state={{
+                    episode: JSON.stringify(episode),
+                    user: {
+                      name,
+                      species,
+                      image,
+                      origin,
+                      location,
+                    },
+                  }}
+                  to={`character/${id}`}
                 >
-                  <Link
-                    state={{
-                      episode: JSON.stringify(episode),
-                      user: {
-                        name,
-                        species,
-                        image,
-                        origin,
-                        location,
-                      },
-                    }}
-                    to={`character/${id}`}
-                  >
-                    <img
-                      alt={name}
-                      src={image}
-                      className="rounded-lg object-cover w-full h-auto"
-                    />
-                    <div className="mt-5 flex gap-2">
-                      <div>
-                        {name ? <p>Name: </p> : null}
-                        {species ? <p>Species: </p> : null}
-                        {gender ? <p>Gender: </p> : null}
-                        {origin.name ? <p>Origin: </p> : null}
-                        {location.name ? <p>Location: </p> : null}
-                      </div>
-                      <div>
-                        {name ? <h4> {name}</h4> : null}
-                        {species ? <p> {species}</p> : null}
-                        {gender ? <p>{gender}</p> : null}
-                        {origin.name ? <p>{origin.name}</p> : null}
-                        {location.name ? <p> {location.name}</p> : null}
-                      </div>
+                  <img
+                    alt={name}
+                    src={image}
+                    className="rounded-lg object-cover w-full h-auto"
+                  />
+                  <div className="mt-5 flex gap-2">
+                    <div>
+                      {name ? <p>Name: </p> : null}
+                      {species ? <p>Species: </p> : null}
+                      {gender ? <p>Gender: </p> : null}
+                      {origin.name ? <p>Origin: </p> : null}
+                      {location.name ? <p>Location: </p> : null}
                     </div>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-          {info ? (
-            <Pagination
-              pageCount={info.count}
-              className="py-10 w-full"
-              pageSize={20}
-              onPageChange={onPageChange}
-            />
-          ) : null}
-        </>
+                    <div>
+                      {name ? <h4> {name}</h4> : null}
+                      {species ? <p> {species}</p> : null}
+                      {gender ? <p>{gender}</p> : null}
+                      {origin.name ? <p>{origin.name}</p> : null}
+                      {location.name ? <p> {location.name}</p> : null}
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       ) : (
         <Loader />
       )}
+      {info ? (
+        <Pagination
+          pageCount={info.count}
+          className="py-10 w-full"
+          pageSize={20}
+          onPageChange={onPageChange}
+        />
+      ) : null}
     </div>
   );
 }
